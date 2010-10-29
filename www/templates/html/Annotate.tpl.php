f0066c9fd841aff8f972314eb325fbe9ee0a8b37
Eric Rasmussen <erasmussen2@unl.edu>
2010-10-29 18:50:57 +0000

    Height/width of the textarea in the iframe isn't available on document.ready/window.load so use a setTimeout as an ugly fix; Some css changes for better webkit styling

--- www/templates/html/Annotate.tpl.php
+++ www/templates/html/Annotate.tpl.php
@@ -5,13 +5,16 @@
 <script type="text/javascript" src="<?php echo UNL_Annotate::getURL(); ?>scripts/annotate_iframe.js"></script>
 <link rel="stylesheet" type="text/css" href="<?php echo UNL_Annotate::getURL(); ?>css/annotate_iframe.css" />
 <script type="text/javascript">
-WDN.jQuery(document).ready(function($){
-	//Adjust for scrollbars, overflow, padding ugliness
-	var height = $('.editRegion textarea').height();
-	var width = $('.editRegion textarea').width();
-	$('.editRegion textarea').height(height-23);
-	$('.editRegion textarea').width(width-5);
-});
+function snugFit()
+{
+    $ = WDN.jQuery;
+    //Adjust for scrollbars, overflow, padding ugliness
+    var height = $('.editRegion textarea').height();
+    var width = $('.editRegion textarea').width();
+    $('.editRegion textarea').height(height-30);
+    $('.editRegion textarea').width(width-10);
+}
+setTimeout('snugFit()',500);
 </script>
 </head>
 <body>
