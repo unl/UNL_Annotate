6c0129c9392c17fcdb5df08f4d271827982b5d2e
Brett Bieber <brett.bieber@gmail.com>
2010-11-16 19:40:46 +0000

    Prevent double entity encoding.

--- www/templates/html/Annotate/Annotation.tpl.php
+++ www/templates/html/Annotate/Annotation.tpl.php
@@ -5,12 +5,12 @@
 <?php else : ?>
 <span class="savelogin">Login to Edit</span>
 <?php endif; ?>
-Your <?php echo $savvy->render($context->sitekey); ?> annotation for <?php echo $savvy->render($context->fieldname); ?>
+Your <?php echo $context->sitekey; ?> annotation for <?php echo $context->fieldname; ?>
 </div>
-<form class="editRegion" id="<?php echo $savvy->render($context->sitekey); ?>_<?php echo $savvy->render($context->fieldname); ?>">
-    <textarea name="note"><?php echo $savvy->render($context->note); ?></textarea>
-    <input type="hidden" name="sitekey" value="<?php echo $savvy->render($context->sitekey); ?>" />
-    <input type="hidden" name="fieldname" value="<?php echo $savvy->render($context->fieldname); ?>" />
+<form class="editRegion" id="<?php echo $context->sitekey; ?>_<?php echo $context->fieldname; ?>">
+    <textarea name="note"><?php echo $context->note; ?></textarea>
+    <input type="hidden" name="sitekey" value="<?php echo $context->sitekey; ?>" />
+    <input type="hidden" name="fieldname" value="<?php echo $context->fieldname; ?>" />
 </form>
 
 <?php if (!UNL_Annotate::getUser()) : ?>
