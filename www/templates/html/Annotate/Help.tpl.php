9f704a5cb43dff3a86c9cf676199b1959dce0bc6
Brett Bieber <brett.bieber@gmail.com>
2011-01-04 14:34:05 +0000

    s/record/person/

--- www/templates/html/Annotate/Help.tpl.php
+++ www/templates/html/Annotate/Help.tpl.php
@@ -2,7 +2,7 @@
 <p>Annotations are your personal notes. After logging in, you can save notes
 for specific content areas around the UNL website. These notes will only
 be viewable by you.</p>
-<p>For example, you can save the spouse's name for a record within the
+<p>For example, you can save the spouse's name for a person within the
 <a href="http://directory.unl.edu/">UNL directory</a>.</p>
 <p>Remember to log out of the My.UNL single sign on service when your session
 has ended.</p>
